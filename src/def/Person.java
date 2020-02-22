2fac8546057f9a412de9b2996d97b41b9a7acf86
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-02-22 19:28:27 +0530

    make getter in class Person

--- src/def/Person.java
+++ src/def/Person.java
@@ -8,10 +8,28 @@ public class Person {
     private String email;
     private LinkedList<String> ContactNumber;
 
+
     public Person(String firstName, String lastName, String email, LinkedList<String> contactNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         ContactNumber = contactNumber;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LinkedList<String> getContactNumber() {
+        return ContactNumber;
+    }
+
 }
