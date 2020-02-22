9605426425c42ac29117514efce9c1671aed7321
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-02-22 18:19:43 +0530

    override methods

---
+++ src/def/MyContact.java
@@ -0,0 +1,29 @@
+package def;
+
+import ADT.MyCon;
+
+import java.util.Scanner;
+
+public class MyContact implements MyCon {
+    Scanner sc = new Scanner(System.in);
+
+    @Override
+    public void addContact() {
+
+    }
+
+    @Override
+    public void viewContact() {
+
+    }
+
+    @Override
+    public void deleteContact() {
+
+    }
+
+    @Override
+    public void searchContact() {
+
+    }
+}
