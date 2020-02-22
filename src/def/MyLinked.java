983e172a364155cb8c652a174a56141012bd42f9
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-02-22 18:29:50 +0530

    make constructor in node class

--- src/def/MyLinked.java
+++ src/def/MyLinked.java
@@ -4,5 +4,10 @@ public class MyLinked {
     private static class Node<E> {
         private E data;
         private Node<E> next;
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
     }
 }
