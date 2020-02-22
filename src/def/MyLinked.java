2c84f47211adaa966d8ca89757caf6595c809b83
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-02-22 18:43:10 +0530

    make addFirst method

--- src/def/MyLinked.java
+++ src/def/MyLinked.java
@@ -1,6 +1,31 @@
 package def;
 
-public class MyLinked {
+import ADT.Cont;
+
+public class MyLinked<E> implements Cont<E> {
+    private Node<E> head = null;
+    private int size = 0;
+
+    private void addFirst(E item) {
+        head = new Node<>(item, head);
+        size++;
+    }
+
+    @Override
+    public void add(Object item) {
+
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E getData(int index) {
+        return null;
+    }
+
     private static class Node<E> {
         private E data;
         private Node<E> next;
