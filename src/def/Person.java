eb6f54d4d49090c4bd20ef62b35bd0508efcb661
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-02-22 19:27:28 +0530

    make Constructor in class Person

--- src/def/Person.java
+++ src/def/Person.java
@@ -1,5 +1,17 @@
 package def;
 
-public class Person {
+import java.util.LinkedList;
 
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LinkedList<String> ContactNumber;
+
+    public Person(String firstName, String lastName, String email, LinkedList<String> contactNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        ContactNumber = contactNumber;
+    }
 }
