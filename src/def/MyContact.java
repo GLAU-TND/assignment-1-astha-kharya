724ce9101fa1853d173ac269e2b4061c2df84f68
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-02-23 19:13:11 +0530

    make GetContactNumber

--- src/def/MyContact.java
+++ src/def/MyContact.java
@@ -3,6 +3,7 @@ package def;
 import ADT.MyCon;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class MyContact implements MyCon {
     Scanner sc = new Scanner(System.in);
@@ -18,6 +19,45 @@ public class MyContact implements MyCon {
         System.out.println("LastName: ");
         String lastName = sc.next();
         return lastName;
+    }
+
+    private MyLinked<String> GetContactNumbers() {
+        MyLinked<String> contactNumbers = new MyLinked<>();
+        System.out.println("ContactNumbers: ");
+        String contactNo = sc.next();
+        while (true) {
+            if (Pattern.matches("[0-9] + ", contactNo)) {
+                contactNumbers.add(contactNo);
+                break;
+            } else {
+                System.out.println("Invalid PhoneNumber");
+                break;
+            }
+        }
+        while (true) {
+
+            System.out.println("Do you want to add a new ContactNumber? (y/n) : ");
+            String a = sc.next();
+            char at = a.charAt(0);
+            if (a.length() > 1) {
+                System.out.println("Please Enter a valid input i.e.,y(lowerCase) for Yes or n(lowerCase for NO)");
+                continue;
+            }
+            if (at == 'y') {
+                System.out.println("Contact Number: ");
+                contactNo = sc.next();
+                if (Pattern.matches("[0-9]+ ", contactNo)) {
+                    contactNumbers.add(contactNo);
+                } else {
+                    System.out.println("Invalid PhoneNumber");
+                }
+            } else if (at == 'n') {
+                break;
+            } else {
+                System.out.println("Please Enter a valid input i.e.,y(lowerCase) for Yes or n(lowerCase for NO)");
+            }
+        }
+        return contactNumbers;
     }
 
     @Override
